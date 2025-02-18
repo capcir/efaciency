32899eb8762667fc14995d23fb5de819a49a72a8
capcir <matthieu.paturet@protonmail.com>
2025-02-18 18:23:15 +0000

    remove astimezone issue in tests

--- tests/test_block.py
+++ tests/test_block.py
@@ -19,41 +19,35 @@ def test_efa_block_to_start_ts():
     assert efaciency.block.to_start_ts(1) == datetime.combine(
         date.today() - timedelta(days=1), time(23)
     ).astimezone(_tz)
-    assert efaciency.block.to_start_ts(2) == datetime.combine(date.today(), time(3)).astimezone(_tz)
-    assert efaciency.block.to_start_ts(3) == datetime.combine(date.today(), time(7)).astimezone(_tz)
-    assert efaciency.block.to_start_ts(4) == datetime.combine(date.today(), time(11)).astimezone(
-        _tz
-    )
-    assert efaciency.block.to_start_ts(5) == datetime.combine(date.today(), time(15)).astimezone(
-        _tz
-    )
-    assert efaciency.block.to_start_ts(6) == datetime.combine(date.today(), time(19)).astimezone(
-        _tz
-    )
+    assert efaciency.block.to_start_ts(2) == datetime.combine(date.today(), time(3), _tz)
+    assert efaciency.block.to_start_ts(3) == datetime.combine(date.today(), time(7), _tz)
+    assert efaciency.block.to_start_ts(4) == datetime.combine(date.today(), time(11), _tz)
+    assert efaciency.block.to_start_ts(5) == datetime.combine(date.today(), time(15), _tz)
+    assert efaciency.block.to_start_ts(6) == datetime.combine(date.today(), time(19), _tz)
 
 
 def test_efa_block_to_end_ts():
-    assert efaciency.block.to_end_ts(1) == datetime.combine(date.today(), time(3)).astimezone(_tz)
-    assert efaciency.block.to_end_ts(2) == datetime.combine(date.today(), time(7)).astimezone(_tz)
-    assert efaciency.block.to_end_ts(3) == datetime.combine(date.today(), time(11)).astimezone(_tz)
-    assert efaciency.block.to_end_ts(4) == datetime.combine(date.today(), time(15)).astimezone(_tz)
-    assert efaciency.block.to_end_ts(5) == datetime.combine(date.today(), time(19)).astimezone(_tz)
-    assert efaciency.block.to_end_ts(6) == datetime.combine(date.today(), time(23)).astimezone(_tz)
+    assert efaciency.block.to_end_ts(1) == datetime.combine(date.today(), time(3), _tz)
+    assert efaciency.block.to_end_ts(2) == datetime.combine(date.today(), time(7), _tz)
+    assert efaciency.block.to_end_ts(3) == datetime.combine(date.today(), time(11), _tz)
+    assert efaciency.block.to_end_ts(4) == datetime.combine(date.today(), time(15), _tz)
+    assert efaciency.block.to_end_ts(5) == datetime.combine(date.today(), time(19), _tz)
+    assert efaciency.block.to_end_ts(6) == datetime.combine(date.today(), time(23), _tz)
 
 
 def test_efa_block_to_ts_with_date():
     assert efaciency.block.to_start_ts(efa_block=1, efa_date=date(2025, 3, 30)) == datetime.combine(
-        date(2025, 3, 29), time(23)
-    ).astimezone(_tz)
+        date(2025, 3, 29), time(23), _tz
+    )
     assert efaciency.block.to_end_ts(efa_block=1, efa_date=date(2025, 3, 30)) == datetime.combine(
-        date(2025, 3, 30), time(3)
-    ).astimezone(_tz)
+        date(2025, 3, 30), time(3), _tz
+    )
     assert efaciency.block.to_start_ts(
         efa_block=1, efa_date=date(2025, 10, 26)
-    ) == datetime.combine(date(2025, 10, 25), time(23)).astimezone(_tz)
+    ) == datetime.combine(date(2025, 10, 25), time(23), _tz)
     assert efaciency.block.to_end_ts(efa_block=1, efa_date=date(2025, 10, 26)) == datetime.combine(
-        date(2025, 10, 26), time(3)
-    ).astimezone(_tz)
+        date(2025, 10, 26), time(3), _tz
+    )
 
 
 def test_efa_block_to_ts_assertion_error():
